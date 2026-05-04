a0cd3da5e759d7e06ad28509ea690fcb626c2df2
Celian <celian.colon@polytechnique.org>
2026-05-04 09:28:03 +0200

    add concatenator

--- scripts/sweep_concatenater.py
+++ scripts/sweep_concatenater.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 name = sys.argv[1]
 
-data_path = os.path.join('..', 'results_sweep')
+data_path = os.path.join('results_sweep')
 files = [f for f in os.listdir(data_path) if name in f and f.endswith(".csv")]
 
 df = pd.concat([pd.read_csv(os.path.join(data_path, f)) for f in files], ignore_index=True)
